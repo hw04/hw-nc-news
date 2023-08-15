a7d9301ce4e154b94dcf134e9722a7aa3710340f
Harry <harryw@posteo.uk>
2023-08-15 14:11:10 +0100

    complete task 3

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { topicsController } = require("./controllers/controller");
+const { topicsController, apiController } = require("./controllers/controller");
 
 const app = express();
 
 app.get("/api/topics", topicsController);
+
+app.get("/api", apiController);
 
 // app.listen(9090, () => console.log("App listening on port 9090!"));
 
