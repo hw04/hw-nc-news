94ffca26a29712af3daa32910c083e9eb76b7507
hw04 <harryw@posteo.uk>
2023-12-03 13:50:58 +0000

    Update app.js formatting

--- app.js
+++ app.js
@@ -1,51 +1,47 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
-const { getTopics } = require("./controllers/topics-controllers");
 
+// Controllers
+const { getTopics } = require("./controllers/topics-controllers");
 const {
   getArticleById,
   getArticleList,
   patchVotes,
 } = require("./controllers/articles-controllers");
-
 const {
   getArticleComments,
   postComment,
   deleteComment,
 } = require("./controllers/comments-controllers");
-
 const { getEndpoints } = require("./controllers/api-controller");
 const { getUsers } = require("./controllers/users-controllers");
-
 const {
   handle400Errors,
   handleCustomErrors,
   handle500Error,
 } = require("./controllers/error-controllers");
 
-const app = express();
-
+// Middleware
 app.use(cors());
-
 app.use(express.json());
 
+// GET requests
 app.get("/api/topics", getTopics);
-
 app.get("/api", getEndpoints);
-
 app.get("/api/articles/:article_id", getArticleById);
-
 app.get("/api/articles", getArticleList);
-
 app.get("/api/articles/:article_id/comments", getArticleComments);
+app.get("/api/users", getUsers);
 
+// POST requests
 app.post("/api/articles/:article_id/comments", postComment);
 
+// PATCH requests
 app.patch("/api/articles/:article_id", patchVotes);
 
+// DELETE requests
 app.delete("/api/comments/:comment_id", deleteComment);
-
-app.get("/api/users", getUsers);
 
 app.use(handle400Errors);
 
